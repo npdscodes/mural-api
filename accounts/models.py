ebd24ceb8c731f8e2b805cdc991c15606767e3b6
Ayron Felipe Carvalho Araujo <ayron41@gmail.com>
2016-10-04 12:02:02 -0300

    Corrigindo erro de digitacao e adicionados os atributos created_at e updated_at ao models

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.core.validators import RegexValidator
 
-from django.cong import settings
+from django.conf import settings
 
 
 class Profile(models.Model):
@@ -25,6 +25,8 @@ class Profile(models.Model):
     email = models.EmailField(max_length=50, unique=True)
     telefone_validator = RegexValidator(regex='^\(\d{2}\) 9?\d{4}-\d{4}$', message=_('O numero telefone deve ser inserido no formato (XX) 9XXXX-XXXX'))
     telefone = models.CharField(max_length=15, validators=[telefone_validator], blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Disciplina(models.Model):
@@ -40,10 +42,14 @@ class Turma(models.Model):
     codigo = models.CharField(max_length=50)
     periodo = models.CharField(max_length=50)
     codigo_ativo = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Inscricao(models.Model):
 
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='minhas_inscricoes')
     turma = models.ForeignKey('Turma', on_delete=models.CASCADE, related_name='alunos')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
