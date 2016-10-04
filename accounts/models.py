e7fdcf554969eb9eb3a5c51903e58bebeb24f655
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-10-04 09:59:36 -0300

    Formatação: Sempre tem um espaço para iniciar a implementação de uma classe. Não precisa de espaço caso tenho o DocString """, Exemplos de Translante por meio da ugettext, Resolvi o lance dos Relacionamentos ManyToOne com relared. E apaguei o relacionamento incorreto. Coloquei um exemplo de __str__.

--- accounts/models.py
+++ accounts/models.py
@@ -2,17 +2,28 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from django.utils.translation import ugettext_lazy as _
+
 
 class Profile(models.Model):
+    """
+    Representa as pessoas no sistema. Dados gerais pessoais. Esta classe servirá de composição para
+    Professor e Aluno dentro do sistema. Relaciona-se
+    com o User do django para fins de autenticação e atutorização.
+    """
+
     SEXO_CHOICES = (
-        (u'masculino', 'Masculino'),
-        (u'feminino', 'Femninino'),
+        ('Masculino', _('Masculino')),
+        ('Feminino', _('Femninino')),
     )
 
-    nome = models.CharField(max_length=100)
-    sexo = models.CharField(max_length=10, choices=SEXO_CHOICES)
+    nome = models.CharField(_('Nome'), max_length=100, null=False, blank=False)
+    sexo = models.CharField(_('Sexo'), max_length=10, choices=SEXO_CHOICES)
     email = models.EmailField(max_length=50)
     telefone = models.BigIntegerField()
+
+    def __str__(self):
+        return "%s - %s" % (self.nome, self.email)
 
 
 class Disciplina(models.Model):
@@ -20,13 +31,12 @@ class Disciplina(models.Model):
 
 
 class Turma(models.Model):
-    alunos = models.ForeignKey('Inscricao', on_delete=models.CASCADE)
-    professor = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    professor = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name="minhas_turmas")
     codigo = models.CharField(max_length=50)
     periodo = models.CharField(max_length=50)
     codigo_ativo = models.BooleanField()
 
 
 class Inscricao(models.Model):
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    turma = models.ForeignKey('Turma', on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='minhas_inscricoes')
+    turma = models.ForeignKey('Turma', on_delete=models.CASCADE, related_name='alunos')
