31203980ee38f1af5a4353d14d02ab679790fe45
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-10-04 08:46:29 -0300

    Formatação: Um espaço(enter) entre imports de fonts diferentes, Substituicao de Tabs por 4 espaços na identação.

--- accounts/models.py
+++ accounts/models.py
@@ -1,28 +1,32 @@
 from django.db import models
+
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
+    SEXO_CHOICES = (
+        (u'masculino', 'Masculino'),
+        (u'feminino', 'Femninino'),
+    )
 
-	SEXO_CHOICES = (
-		(u'masculino', 'Masculino'),
-		(u'feminino', 'Femninino'),
-	)
+    nome = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=10, choices=SEXO_CHOICES)
+    email = models.EmailField(max_length=50)
+    telefone = models.BigIntegerField()
 
-	nome = models.CharField(max_length=100)
-	sexo = models.CharField(max_length=10, choices=SEXO_CHOICES)
-	email = models.EmailField(max_length=50)
-	telefone = models.BigIntegerField()
 
 class Disciplina(models.Model):
-	nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+
 
 class Turma(models.Model):
-	alunos = models.ForeignKey('Inscricao', on_delete=models.CASCADE)
-	professor = models.ForeignKey('Profile', on_delete=models.CASCADE)
-	codigo = models.CharField(max_length=50)
-	periodo = models.CharField(max_length=50)
-	codigo_ativo = models.BooleanField()
+    alunos = models.ForeignKey('Inscricao', on_delete=models.CASCADE)
+    professor = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=50)
+    periodo = models.CharField(max_length=50)
+    codigo_ativo = models.BooleanField()
+
 
 class Inscricao(models.Model):
-	pro = models.ForeignKey('Profile', on_delete=models.CASCADE)
-	tur = models.ForeignKey('Turma', on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    turma = models.ForeignKey('Turma', on_delete=models.CASCADE)
