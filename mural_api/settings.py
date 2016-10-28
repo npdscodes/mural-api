e0c5aeacedb8989b464b523a0512a2c45013cb0c
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-10-28 11:08:31 -0300

    Ajuste no static

--- mural_api/settings.py
+++ mural_api/settings.py
@@ -122,6 +122,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
