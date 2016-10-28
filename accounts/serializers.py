8e33105480e3f216298658435e133960f34f48f3
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-10-28 08:19:10 -0300

    Correcao dos serializers. Inclusao de Campos PK Related. Inclusao de mais alguns fieds de leitura. Formatacao PEP-8

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,16 +8,14 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     full_name = serializers.CharField(source='get_full_name', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
 
 
 class PerfilSerializer(serializers.ModelSerializer):
-
     usuario = serializers.SlugRelatedField(slug_field=User.USERNAME_FIELD, queryset=User.objects.all())
 
     class Meta:
@@ -27,25 +25,30 @@ class PerfilSerializer(serializers.ModelSerializer):
 
 class DisciplinaSerializer(serializers.ModelSerializer):
 
-	class Meta:
-		model = Disciplina
-		fields = ('id', 'nome')
+    qtd_inscritos = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Disciplina
+        fields = ('id', 'nome', 'qtd_inscritos')
+
+    def get_qtd_inscritos(self, obj):
+        qtd = Disciplina.objects.filter(pk=obj.pk).count()
+        return qtd
+
 
 class TurmaSerializer(serializers.ModelSerializer):
-    
-    professor = serializers.SlugRelatedField(slug_field=Perfil.nome, queryset=Perfil.objects.all())
+    professor = serializers.PrimaryKeyRelatedField(many=False, queryset=Perfil.objects.all())
+    disciplina = serializers.PrimaryKeyRelatedField(many=False, queryset=Disciplina.objects.all())
 
     class Meta:
         model = Turma
-        fields = ('id', 'professor', 'codigo', 'periodo', 'codigo_ativo',)
+        fields = ('id', 'codigo', 'periodo', 'codigo_ativo', 'professor', 'disciplina')
 
 
 class InscricaoSerializer(serializers.ModelSerializer):
-
-    perfil = serializers.SlugRelatedField(slug_field=Perfil.nome, queryset=Perfil.objects.all())
-    turma = serializers.SlugRelatedField(slug_field=Turma.codigo, queryset=Turma.objects.all())
+    perfil = serializers.PrimaryKeyRelatedField(many=False, queryset=Perfil.objects.all())
+    turma = serializers.PrimaryKeyRelatedField(many=False, queryset=Turma.objects.filter(codigo_ativo=True))
 
     class Meta:
         model = Inscricao
-        fields = ('id', 'perfil', 'turma',)
-
+        fields = ('id', 'perfil', 'turma', 'criado_em')
