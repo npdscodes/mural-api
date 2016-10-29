3477ebccf1a1b60d5f5c9642dbdf8a8d7b6f9235
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2016-10-29 12:37:27 -0300

    PAGINATION: Remocao e renomeacao do MIXIN que cuida de filtro, pagina, permi, auth. Para desabilitar paginacao em uma viewset basta settar pagination_class = None

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from rest_framework import authentication, permissions, filters
+
 
 class CriacaoEAtualizacaoMixin(models.Model):
     """
@@ -10,3 +12,24 @@ class CriacaoEAtualizacaoMixin(models.Model):
 
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
+
+
+class ViewsetPadraoMixin(object):
+    """Default settings for view authentication, permissions, filtering and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -1,36 +1,18 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets, authentication, permissions, filters
+from rest_framework import viewsets
 
 from .serializers import UserSerializer, PerfilSerializer, DisciplinaSerializer, TurmaSerializer, InscricaoSerializer
 
 from .models import Perfil, Disciplina, Turma, Inscricao
 
+from .mixins import ViewsetPadraoMixin
+
 
 User = get_user_model()
 
 
-class DefaultsMixin(object):
-    """Default settings for view authentication, permissions, filtering and pagination."""
-
-    authentication_classes = (
-        authentication.BasicAuthentication,
-        authentication.TokenAuthentication,
-    )
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
-    paginate_by = 25
-    paginate_by_param = 'page_size'
-    max_paginate_by = 100
-    filter_backends = (
-        filters.DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter,
-    )
-
-
-class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+class UserViewSet(ViewsetPadraoMixin, viewsets.ReadOnlyModelViewSet):
     """API endpoint for listing users."""
 
     lookup_field = User.USERNAME_FIELD
@@ -38,10 +20,11 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer
     search_fields = (User.USERNAME_FIELD,)
+    pagination_class = None
     ordering_fields = ('username', 'first_name', 'last_name', 'email')
 
 
-class PerfilViewSet(DefaultsMixin, viewsets.ModelViewSet):
+class PerfilViewSet(ViewsetPadraoMixin, viewsets.ModelViewSet):
     """Endpoint para a criação e listagem de perfis"""
 
     queryset = Perfil.objects.order_by('nome')
@@ -50,7 +33,7 @@ class PerfilViewSet(DefaultsMixin, viewsets.ModelViewSet):
     ordering_fields = ('nome', 'email',)
 
 
-class DisciplinaViewSet(DefaultsMixin, viewsets.ModelViewSet):
+class DisciplinaViewSet(ViewsetPadraoMixin, viewsets.ModelViewSet):
     """Endpoint para a criação e listagem das disciplinas"""
 
     queryset = Disciplina.objects.all()
@@ -59,7 +42,7 @@ class DisciplinaViewSet(DefaultsMixin, viewsets.ModelViewSet):
     ordering_fields = ('nome',)
 
 
-class TurmaViewSet(DefaultsMixin, viewsets.ModelViewSet):
+class TurmaViewSet(ViewsetPadraoMixin, viewsets.ModelViewSet):
     """Endpoint para a criação e listagem das turmas"""
 
     queryset = Turma.objects.order_by('codigo')
@@ -68,7 +51,7 @@ class TurmaViewSet(DefaultsMixin, viewsets.ModelViewSet):
     ordering_fields = ('codigo',)
 
 
-class InscricaoViewSet(DefaultsMixin, viewsets.ModelViewSet):
+class InscricaoViewSet(ViewsetPadraoMixin, viewsets.ModelViewSet):
     """Endpoint para a criação e listagem das inscrições"""
 
     queryset = Inscricao.objects.all()
