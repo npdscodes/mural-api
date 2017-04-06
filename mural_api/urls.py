184968b6fd785cabe13ef9b7ee42d7da90a63f2a
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2017-04-06 08:41:41 -0300

    AJustes de URL,inclusao da versao(v1)

--- mural_api/urls.py
+++ mural_api/urls.py
@@ -25,7 +25,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='MURAL API')
 
 urlpatterns = [
-    url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/v1/token/', obtain_auth_token, name='api-token'),
     url(r'^api/v1/', include(router.urls)),
-    url(r'^api/docs/$', schema_view),
+    url(r'^api/v1/docs/$', schema_view),
 ]
